8e445a159da81542ae3f2f97787f1493074f0316
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2024-11-15 20:54:34 +0330

    Fix traceroute test interface problem

--- internal/tests/traceroute.go
+++ internal/tests/traceroute.go
@@ -20,7 +20,6 @@ func RunTraceroute(host string) (TracerouteResult, error) {
 	if err != nil {
 		return TracerouteResult{}, fmt.Errorf("failed to resolve host: %w", err)
 	}
-
 	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
 	if err != nil {
 		return TracerouteResult{}, fmt.Errorf("failed to listen for ICMP: %w", err)
@@ -29,29 +28,37 @@ func RunTraceroute(host string) (TracerouteResult, error) {
 
 	hops := []string{}
 	for ttl := 1; ttl <= maxHops; ttl++ {
-		p := ipv4.NewPacketConn(c)
-		p.SetTTL(ttl)
+		if err := c.IPv4PacketConn().SetTTL(ttl); err != nil {
+			hops = append(hops, fmt.Sprintf("%d * **", ttl))
+			continue
+		}
 
 		msg := icmp.Message{
 			Type: ipv4.ICMPTypeEcho, Code: 0,
 			Body: &icmp.Echo{ID: ttl, Seq: ttl, Data: []byte("traceroute")},
 		}
-
 		start := time.Now()
-		msgBytes, _ := msg.Marshal(nil)
-		c.WriteTo(msgBytes, ipAddr)
+		msgBytes, err := msg.Marshal(nil)
+		if err != nil {
+			return TracerouteResult{}, fmt.Errorf("failed to marshal ICMP message: %w", err)
+		}
+
+		_, err = c.WriteTo(msgBytes, ipAddr)
+		if err != nil {
+			hops = append(hops, fmt.Sprintf("%d * **", ttl))
+			continue
+		}
 
 		reply := make([]byte, 1500)
 		c.SetReadDeadline(time.Now().Add(time.Second))
 		n, peer, err := c.ReadFrom(reply)
 		if err != nil {
-			hops = append(hops, fmt.Sprintf("%d * * *", ttl))
+			hops = append(hops, fmt.Sprintf("%d * **", ttl))
 			continue
 		}
 
 		rtt := time.Since(start)
 		hops = append(hops, fmt.Sprintf("%d %v %v", ttl, peer, rtt))
-
 		parsedMsg, _ := icmp.ParseMessage(1, reply[:n])
 		if parsedMsg.Type == ipv4.ICMPTypeEchoReply {
 			break
